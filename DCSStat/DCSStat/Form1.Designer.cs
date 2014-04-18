2b7b8d1246b38ae19e9460ab052026deb4c20d1b
storm363 <steuku@gmail.com>
2014-04-18 16:40:16 +0200

    Updated logic

--- DCSStat/DCSStat/Form1.Designer.cs
+++ DCSStat/DCSStat/Form1.Designer.cs
@@ -41,6 +41,15 @@
             this.labeject = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // timer1
@@ -52,11 +61,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 79);
+            this.label1.Location = new System.Drawing.Point(89, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "nickname";
             // 
             // listBox1
             // 
@@ -64,43 +72,40 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(25, 95);
+            this.listBox1.Location = new System.Drawing.Point(15, 283);
             this.listBox1.Name = "listBox1";
             this.listBox1.ScrollAlwaysVisible = true;
-            this.listBox1.Size = new System.Drawing.Size(238, 147);
+            this.listBox1.Size = new System.Drawing.Size(337, 147);
             this.listBox1.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(71, 42);
+            this.label2.Location = new System.Drawing.Point(89, 119);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.Size = new System.Drawing.Size(0, 13);
             this.label2.TabIndex = 0;
-            this.label2.Text = "Status";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(125, 66);
+            this.label3.Location = new System.Drawing.Point(89, 53);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.Size = new System.Drawing.Size(0, 13);
             this.label3.TabIndex = 0;
-            this.label3.Text = "IP";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(166, 79);
+            this.label4.Location = new System.Drawing.Point(89, 75);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.Size = new System.Drawing.Size(0, 13);
             this.label4.TabIndex = 0;
-            this.label4.Text = "Color";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(196, 33);
+            this.label5.Location = new System.Drawing.Point(89, 186);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(45, 13);
             this.label5.TabIndex = 0;
@@ -109,43 +114,121 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 9);
+            this.label6.Location = new System.Drawing.Point(89, 31);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(65, 13);
+            this.label6.Size = new System.Drawing.Size(0, 13);
             this.label6.TabIndex = 0;
-            this.label6.Text = "server name";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(196, 55);
+            this.label7.Location = new System.Drawing.Point(89, 164);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(33, 13);
+            this.label7.Size = new System.Drawing.Size(0, 13);
             this.label7.TabIndex = 0;
-            this.label7.Text = "plane";
             // 
             // labeject
             // 
             this.labeject.AutoSize = true;
-            this.labeject.Location = new System.Drawing.Point(261, 33);
+            this.labeject.Location = new System.Drawing.Point(89, 141);
             this.labeject.Name = "labeject";
-            this.labeject.Size = new System.Drawing.Size(30, 13);
+            this.labeject.Size = new System.Drawing.Size(0, 13);
             this.labeject.TabIndex = 0;
-            this.labeject.Text = "eject";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(261, 9);
+            this.label8.Location = new System.Drawing.Point(89, 97);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(26, 13);
+            this.label8.Size = new System.Drawing.Size(0, 13);
             this.label8.TabIndex = 0;
-            this.label8.Text = "time";
             // 
             // timer2
             // 
             this.timer2.Interval = 1000;
             this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(55, 13);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Nickname";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 31);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(69, 13);
+            this.label10.TabIndex = 3;
+            this.label10.Text = "Server Name";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(12, 75);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(47, 13);
+            this.label11.TabIndex = 4;
+            this.label11.Text = "Coalition";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(12, 97);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(33, 13);
+            this.label12.TabIndex = 5;
+            this.label12.Text = "Time ";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(12, 119);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(37, 13);
+            this.label13.TabIndex = 6;
+            this.label13.Text = "Status";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(12, 141);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(31, 13);
+            this.label14.TabIndex = 7;
+            this.label14.Text = "Eject";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(12, 164);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(34, 13);
+            this.label15.TabIndex = 8;
+            this.label15.Text = "Plane";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(12, 53);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(51, 13);
+            this.label16.TabIndex = 9;
+            this.label16.Text = "Server IP";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(189, 250);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(41, 13);
+            this.label17.TabIndex = 10;
+            this.label17.Text = "label17";
             // 
             // Form1
             // 
@@ -153,6 +236,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(364, 442);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.labeject);
@@ -185,6 +277,15 @@
         private System.Windows.Forms.Label labeject;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
     }
 }
 
--- DCSStat/DCSStat/Form1.cs
+++ DCSStat/DCSStat/Form1.cs
@@ -9,127 +9,177 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Diagnostics;
+using MySql.Data.MySqlClient;
 
 namespace DCSStat
 {
     public partial class Form1 : Form
     {
-        string temp = null,nickname= "nick",statuscon="offline",ipserver=null,colorplayers="gray";
+        string temp = null, nickname= "nick", statuscon="offline", ipserver=null, colorplayers="gray";
         bool land = true;
-        int crush = 0,eject=0,sec=0,min=0,chas=0;
+        int crush = 0,eject=0,sec=0,min=0,chas=0, dbCounter = 0;
         int tlen = 1;
+
+        //стринги для бд
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        //метод для обновления базы данных
+        private void dbUpdate()
+        {
+            server = "94.249.139.6";
+            database = "dcssta_dcs";
+            uid = "dcssta_dcs";
+            password = "P@ssw0rd";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            connection = new MySqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                if (connection.State.Equals(ConnectionState.Closed))
+                {
+                    MessageBox.Show("Succesfully connected!");
+                }
+                
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
         private void timer1_Tick(object sender, EventArgs e)
         {
-            File.Copy(@"C:\Users\Gena\Saved Games\DCS\Logs\dcs.log", @"C:\Users\Gena\Saved Games\DCS\Logs\dcss.ds", true);
-            StreamReader sr = new StreamReader(@"C:\Users\Gena\Saved Games\DCS\Logs\dcss.ds");
-            string [] a =sr.ReadToEnd().Split('\n');
-            for (int i = tlen-1; i < a.Length -1; i++)
+            //Если процесс есть, идем дальше, если нет, пишем что процесса нет, ждем..
+            Process[] pname = Process.GetProcessesByName("DCS");
+            if (pname.Length != 0)
             {
-                listBox1.Items.Add(i.ToString()+ "    "+a[i]);
-                Regex r = new Regex(".*name=");
-                Regex r1 = new Regex(".*IPservera:");
-                Regex r3 = new Regex(".*motd=");
-                if (a[i].Contains("IPservera:"))
-                {     
-                    ipserver = r1.Replace(a[i].Replace("\"", ""), "");
-                    label3.Text = ipserver;
-                }
-                else if (a[i].Contains("connected to server"))
+                File.Copy(Environment.ExpandEnvironmentVariables(@"%UserProfile%\Saved Games\DCS.openbeta\Logs\dcs.log"), Environment.ExpandEnvironmentVariables(@"%UserProfile%\Saved Games\DCS.openbeta\Logs\dcss.ds"), true);
+                StreamReader sr = new StreamReader(Environment.ExpandEnvironmentVariables(@"%UserProfile%\Saved Games\DCS.openbeta\Logs\dcss.ds"));
+                string[] a = sr.ReadToEnd().Split('\n');
+                for (int i = tlen - 1; i < a.Length - 1; i++)
                 {
-                    statuscon = "online";
-                    label2.Text = "online"; label2.ForeColor = Color.Green;
-                }
-                else if (a[i].Contains("PlayerStop"))
-                {
-                    statuscon = "offline";
-                    label2.Text = "offline";
-                    label2.ForeColor = Color.Red;
-                    land = true;
-                }
-                else if (a[i].Contains("ParametrServ: motd="))
-                {
-                    label6.Text = (r3.Replace(a[i].Replace("\"", ""), "")).Trim();
-                   
-                }
-                else if(a[i].Contains("DCSStat_name="))
-                {
-                    nickname = (r.Replace(a[i].Replace("\"", ""), "")).Trim();
-                    label1.Text = nickname;
-                 }
-                else if (a[i].Contains("playerfly"))
-                {
-                    land = false;
-                }
-                else if (a[i].Contains("playerground"))
-                {
-                    land = true;
-                }
-    
-                else if (a[i].Contains("DCSStat_crush"))
-                {
-                    Regex r2 = new Regex(".*игрок");
-                    if (nickname == (r2.Replace(a[i].Replace("\"", ""), "")).Trim())
+                    listBox1.Items.Add(i.ToString() + "    " + a[i]);
+                    Regex r = new Regex(".*name=");
+                    Regex r1 = new Regex(".*IPservera:");
+                    Regex r3 = new Regex(".*motd=");
+                    if (a[i].Contains("IPservera:"))
                     {
-                        crush++;
-                        label5.Text = crush.ToString();
-                        timer2.Stop();
+                        ipserver = r1.Replace(a[i].Replace("\"", ""), "");
+                        label3.Text = ipserver;
+                    }
+                    else if (a[i].Contains("connected to server"))
+                    {
+                        statuscon = "Online";
+                        label2.Text = "Online"; label2.ForeColor = Color.Green;
+                    }
+                    else if (a[i].Contains("PlayerStop"))
+                    {
+                        statuscon = "Offline";
+                        label2.Text = "Offline";
+                        label2.ForeColor = Color.Red;
                         land = true;
                     }
-                }
-                else if (a[i].Contains("DCSStat_info=\""+nickname))
-                {
-                    Regex r4 = new Regex(".*DCSStat_plane=");
-                    
-                  
-                        label7.Text = (r4.Replace(a[i].Replace("\"", ""), "")).Trim();
-                   
-                }
-                else if (a[i].Contains("DCSStat_eject"))
-                {
-                    Regex r5 = new Regex(".*игрок");
-                    if (nickname == (r5.Replace(a[i].Replace("\"", ""), "")).Trim())
+                    else if (a[i].Contains("ParametrServ: motd="))
                     {
-                        eject++;
-                       labeject.Text = eject.ToString();
-                       land = true;
+                        label6.Text = (r3.Replace(a[i].Replace("\"", ""), "")).Trim();
+
+                    }
+                    else if (a[i].Contains("DCSStat_name="))
+                    {
+                        nickname = (r.Replace(a[i].Replace("\"", ""), "")).Trim();
+                        label1.Text = nickname;
+                    }
+                    else if (a[i].Contains("playerfly"))
+                    {
+                        land = false;
+                    }
+                    else if (a[i].Contains("playerground"))
+                    {
+                        land = true;
+                    }
+
+                    else if (a[i].Contains("DCSStat_crush"))
+                    {
+                        Regex r2 = new Regex(".*игрок");
+                        if (nickname == (r2.Replace(a[i].Replace("\"", ""), "")).Trim())
+                        {
+                            crush++;
+                            label5.Text = crush.ToString();
+                            timer2.Stop();
+                            land = true;
+                        }
+                    }
+                    else if (a[i].Contains("DCSStat_info=\"" + nickname))
+                    {
+                        Regex r4 = new Regex(".*DCSStat_plane=");
+
+
+                        label7.Text = (r4.Replace(a[i].Replace("\"", ""), "")).Trim();
+
+                    }
+                    else if (a[i].Contains("DCSStat_eject"))
+                    {
+                        Regex r5 = new Regex(".*игрок");
+                        if (nickname == (r5.Replace(a[i].Replace("\"", ""), "")).Trim())
+                        {
+                            eject++;
+                            labeject.Text = eject.ToString();
+                            land = true;
+                        }
+                    }
+                    else if (a[i].Contains(nickname + "\" took off"))
+                    {
+                        land = false;
+                    }
+                    else if (a[i].Contains(nickname + "\" landed"))
+                    {
+                        land = true;
+                        timer2.Stop();
+                    }
+                    else if (a[i].Contains("КРАСНЫЙ игрок \"" + nickname.Trim() + "\""))
+                    {
+                        colorplayers = "RED";
+                        label4.Text = colorplayers;
+                        label4.ForeColor = Color.Red;
+                    }
+                    else if (a[i].Contains("СИНИЙ игрок \"" + nickname.Trim() + "\""))
+                    {
+                        colorplayers = "BLUE";
+                        label4.Text = colorplayers;
+                        label4.ForeColor = Color.Blue;
                     }
                 }
-                else if (a[i].Contains(nickname+"\" took off"))
-                {
-                    land = false;
-                }
-                else if (a[i].Contains(nickname + "\" landed"))
-                {
-                    land = true;
+                if (land == false)
+                    timer2.Start();
+                else
                     timer2.Stop();
-                }
-                else if (a[i].Contains("КРАСНЫЙ игрок \""+nickname.Trim()+"\""))
-                {
-                    colorplayers = "RED";
-                    label4.Text = colorplayers;
-                    label4.ForeColor = Color.Red;
-                }
-                else if (a[i].Contains("СИНИЙ игрок \""+nickname.Trim()+"\""))
-                {
-                    colorplayers = "BLUE";
-                    label4.Text = colorplayers;
-                    label4.ForeColor = Color.Blue;
-                }
-
-
-            }
-            if (land == false)
-                timer2.Start();
-            else
-                timer2.Stop();
-               sr.Close();
+                sr.Close();
                 tlen = a.Length;
-             
+                //добавляем у счктчику 10 мс, когда будет 5000, обновляем базу данных
+                dbCounter += 10;
+                label17.Text = dbCounter.ToString();
+                if (dbCounter == 5000)
+                {
+                    //dbUpdate();
+                    dbCounter = 0;
+                }
+            }
+            else
+            {
+                label2.ForeColor = Color.Red;   
+                label2.Text = "DCS is not started!";
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -167,6 +217,11 @@ namespace DCSStat
                 chaso = "";
 
             label8.Text = string.Format("Время в полёте {0}{1}:{2}{3}:{4}{5}",chaso, chas,mino, min,seco, sec);
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
 
         }
     }
